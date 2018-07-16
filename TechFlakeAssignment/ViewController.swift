e18f4d35d7ad1191fa073bdd82c50a0d2d847e76
Jayesh kanzariya <jayeshkanzariya28@gmail.com>
2018-07-16 20:24:17 +0530

    Initial Commit

---
+++ TechFlakeAssignment/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TechFlakeAssignment
+//
+//  Created by Jayesh on 16/07/18.
+//  Copyright © 2018 Jayesh kanzariya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
